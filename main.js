b506a85bea5752b27019f76ab41e8e9209c500ff
harkisa9 <harkisa9@uw.edu>
2021-10-04 11:17:05 -0400

    add and remove elements in list

--- main.js
+++ main.js
@@ -2,3 +2,17 @@ function addNewList() {
     alert('hello world alert!');
     console.log('hello world console');
 }
+
+function addListItem() {
+    console.log('hello world');
+    let list = document.getElementById("grocery-list");
+    let itemInput = document.getElementById("text_input");
+    let newItem = document.createElement("li");
+    newItem.appendChild(document.createTextNode(itemInput.value));
+    list.appendChild(newItem);
+}
+
+function deleteListItem(item) {
+    // remove li element (item) from ol element (item.parentNode)
+     item.parentNode.removeChild(item);
+}
